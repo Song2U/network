ba23267f7f463862a9363a15cfe25e7bb67f529b
S401-07 <S401-07@S401-07>
2016-07-19 11:01:12 +0900

    EchoServer/Client 주석추가 & 보조스트림 사용 소스 추가

--- network/src/kr/ac/sungkyul/network/echo/EchoClient2.java
+++ network/src/kr/ac/sungkyul/network/echo/EchoClient2.java
@@ -1,19 +1,24 @@
 package kr.ac.sungkyul.network.echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.Scanner;
 
-public class EchoClient {
+public class EchoClient2 {
 	private static final String SERVER_IP = "220.67.115.224";
 	private static final int SERVER_PORT = 1000;
 
 	public static void main(String[] args) {
 		Socket socket = null;
+		Scanner scanner = new Scanner(System.in);
 		try { // 1. 소켓 생성
 			socket = new Socket();
 
@@ -21,28 +26,36 @@ public class EchoClient {
 			InetSocketAddress serverSocketAddress = new InetSocketAddress(SERVER_IP, SERVER_PORT);
 			socket.connect(serverSocketAddress);
 
-			// 3. IOStream 받아오기
-			InputStream is = socket.getInputStream();
-			OutputStream os = socket.getOutputStream();
+			// 3. IOStream 받아오기 (보조스트림으로 씌우기)
+			// InputStream is = socket.getInputStream();
+			// OutputStream os = socket.getOutputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
 
-			Scanner scanner = new Scanner(System.in);
 			// 5. 데이터 읽기
 			while (true) {
 				System.out.print(">> ");
+
 				String data = scanner.nextLine(); // 문자열 입력을 읽어냄
-				if (data.equals("exit")) { // exit 문자열이 들어오면 프로그램 종료
+				if ("exit".equals(data)) { // exit 문자열이 들어오면 프로그램 종료
 					break;
 				}
-				os.write(data.getBytes("utf-8"));
-				byte[] buffer = new byte[256];
-				int readBytes = is.read(buffer); // blocking, 몇 바이트를 읽었는지 체크
-				if (readBytes <= -1) { // 서버가 연결을 끊음
+				// os.write(data.getBytes("utf-8"));
+				// byte[] buffer = new byte[256];
+				// int readBytes = is.read(buffer); // blocking, 몇 바이트를 읽었는지 체크
+				// 메시지 보내기
+				pw.println(data);
+				// 메세지 다시 받기
+				String dataEcho = br.readLine();
+				if (dataEcho == null) { // 서버가 연결을 끊음
 					System.out.println("[Client] closed by Server");
-					return;
+					break;
 				}
-				data = new String(buffer, 0, readBytes, "utf-8");
-
+				// data = new String(buffer, 0, readBytes, "utf-8");
+				
+				
 				System.out.println("<< " + data);
+				
 			}
 		} catch (SocketException e) {
 			System.out.println("[Client] 비정상적으로 서버로부터 연결이 끊어졌습니다.");
--- network/src/kr/ac/sungkyul/network/echo/EchoServer2.java
+++ network/src/kr/ac/sungkyul/network/echo/EchoServer2.java
@@ -1,15 +1,19 @@
 package kr.ac.sungkyul.network.echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
-public class EchoServer {
+public class EchoServer2 {
 	private final static int SERVER_PORT = 1000;
 
 	public static void main(String[] args) {
@@ -31,23 +35,33 @@ public class EchoServer {
 			System.out.println("<EchoServer> 연결 성공, from " + remoteHostAddress + ":" + remoteAddress.getPort());
 			try {
 				// 5. IOStream
-				InputStream is = socket.getInputStream();
-				OutputStream os = socket.getOutputStream();
+				/*
+				 * InputStream is = socket.getInputStream(); OutputStream os =
+				 * socket.getOutputStream();
+				 */
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
 				// 6. 데이터 읽기
 				while (true) {
-					byte[] buffer = new byte[256]; // 메세지가 입력되는 바이트단위의 버퍼 준비
-					int readBytes = is.read(buffer); // blocked (바이트 단위로 읽음)
-					if (readBytes <= -1) { // 클라이언트가 연결을 끊음(정상 종료)
+					/*
+					 * byte[] buffer = new byte[256]; // 메세지가 입력되는 바이트단위의 버퍼 준비
+					 * int readBytes = is.read(buffer); // blocked (바이트 단위로 읽음)
+					 */
+					String data = br.readLine();
+					if (data == null) { // 클라이언트가 연결을 끊음(정상 종료)
 						System.out.println("<EchoServer> closed by client");
 						return;
 					}
 
 					// 출력
-					String data = new String(buffer, 0, readBytes, "utf-8");
+					// String data = new String(buffer, 0, readBytes, "utf-8");
 					System.out.println("<EchoServer> received : " + data);
 					// 7. 데이터 쓰기
-					os.write(data.getBytes("utf-8")); // os.write(buffer);
+					// os.write(data.getBytes("utf-8")); // os.write(buffer);
+					
+					pw.println(data);
+					
 				}
 			} catch (SocketException e) {
 				System.out.println("<EchoServer> 비정상적으로 클라이언트가 연결을 끊었습니다.");
